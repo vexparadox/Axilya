cddfcfab26ed272953b2d36d98748002742024da
William Meaton <will.meaton@gmail.com>
2016-05-20 11:03:37 +0100

    Test build with Travis

--- gamesrc/Core.cpp
+++ gamesrc/Core.cpp
@@ -19,21 +19,25 @@ void Core::setup(){
     setBackground(255, 255, 255, 255);
 	scene1 = new Scene();
 	Runner::setActiveScene(scene1);
+	
 	Entity* e = new Entity(50, 50, 50 ,50);
 	e->addComponent(new RigidBody(e, true));
 	e->addTexture(new Image("img.png"));
 	e->addCollider(new BoxCollider(e));
 	scene1->addEntity(e);
+	
 	e = new Entity(300, 50, 50 ,50);
 	e->addComponent(new RigidBody(e, true));
 	e->addTexture(new Image("img.png"));
 	e->addCollider(new BoxCollider(e));
 	scene1->addEntity(e);
+	
 	e = new Entity(200, 60, 20, 20);
 	e->addComponent(new RigidBody(e, true));
 	e->addTexture(new Image("img.png"));
 	e->addCollider(new BoxCollider(e));
 	scene1->addEntity(e);
+	
 	std::cout << scene1->numEntities()<<std::endl;
 	
 }
