98cc6ab379242606f5117655e8e14dbaf03a5ce7
William Meaton <will.meaton@gmail.com>
2016-05-23 22:17:31 +0100

    Moved main.cpp to the gamesrc

--- gamesrc/CoreMain.h
+++ gamesrc/CoreMain.h
@@ -16,5 +16,6 @@
 #include "../cfsrc/Entity.hpp"
 #include "../cfsrc/Scene.hpp"
 #include "../cfsrc/Input.hpp"
+#include "Core.hpp"
 
 #endif /* CoreMain_h */
--- gamesrc/main.cpp
+++ gamesrc/main.cpp
@@ -5,8 +5,7 @@
 //  Created by William Meaton on 07/12/2015.
 //  Copyright © 2015 WillMeaton.uk. All rights reserved.
 //
-#include "Runner.hpp"
-#include "../gamesrc/Core.hpp"
+#include "CoreMain.h"
 
 int main(int argc, char *argv[])
 {
