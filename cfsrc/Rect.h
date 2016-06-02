ef5c80889a0b64fbb76a81f3f4fdda029a37f61c
William Meaton <will.meaton@gmail.com>
2016-06-02 17:26:57 +0100

    Started doxygen documenting

--- cfsrc/Rect.h
+++ cfsrc/Rect.h
@@ -26,13 +26,23 @@ namespace Graphics{
         }
         
         void set(const Math::Vector2D &v, float w, float h){
-            this->position = v;
+            this->set(v.x, v.y, w, h);
+        }
+        /*
+       * A method to set the Rect's parameters
+       * @param x a float to set the x value to
+       * @param y a float to set the y value to
+       * @param w a float to set the width to
+       * @param h a float to set the height to
+       * @see 
+       * @return void
+       */
+
+        void set(float x, float y, float w, float h){
+            this->position.x = x;
+            this->position.y = y;
             this->w = w;
             this->h = h;
-        }
-        
-        void set(float x, float y, float w, float h){
-            this->set(Math::Vector2D(x, y), w, h);
         }
         
         const float getX() const{
