806ed378b43f95316391811cbfb0d82a8a7ac4fb
William Meaton <will.meaton@gmail.com>
2016-07-07 14:24:16 +0100

    Added example of changing the colour

--- gamesrc/myComponents/exampleComponent.cpp
+++ gamesrc/myComponents/exampleComponent.cpp
@@ -10,11 +10,15 @@ void exampleComponent::update(){
         //this line gets a pointer to the rigidBody attached to the owner
         //it then adds an upwards force to it, the rigidBody handles the rest 
 		owner->getRigidBody()->addForce(0, -10);
+        //set the colour of the entity
+        owner->setColour(0, 255, 255, 255);
 	}
     if(Input::keyRight()){
+        owner->setColour(255, 0, 255, 255);
 		owner->getRigidBody()->addForce(2, 0);
 	}
     if(Input::keyLeft()){
+        owner->setColour(0, 255, 0, 255);
 		owner->getRigidBody()->addForce(-2, 0);
 	}
 }
