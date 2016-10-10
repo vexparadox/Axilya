25211a6fed51827edb325fafbb9de9097e89f72d
William Meaton <will.meaton@gmail.com>
2016-10-10 14:50:48 +0100

    added comments about different coreS

--- gamesrc/main.cpp
+++ gamesrc/main.cpp
@@ -10,6 +10,9 @@
 int main(int argc, char *argv[])
 {
     //start a new runner with the constructor
-    //Runner(int width, int height, int frameRate, const char* title, new Core);
+    //Runner(int width, int height, int frameRate, const char* title, new "corename");
+    //there are two example cores given with the repo, frameworkCore and engineCore
+    //framework core is for showing off the simple framework code
+    //engine core is for showing off the ECS and engine code
     Runner(720, 480, 60, "closedFrameworks", new frameworkCore());
 }
