aa883838adb94cc0f1247f56dd842f81c027a448
William Meaton <will.meaton@gmail.com>
2017-01-31 14:18:33 +0000

    removed old build script

--- Examples/Basic/Build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ "$OSTYPE" == darwin* ]]; then
-	echo "Building Basic example for OSX.."
-	g++ *.cpp -std=c++11 -o Axilya -lAxilya -framework SDL2
-	./Axilya
-	echo "Basic Example complete"
-elif [[ "$OSTYPE" == "linux-gnu"  ]]; then
-	echo "Examples don't work on Linux yet"
-fi
