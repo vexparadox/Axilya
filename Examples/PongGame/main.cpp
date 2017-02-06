853d26c39095339e9baa89474c3704a1c51c7afb
William Meaton <will.meaton@gmail.com>
2017-02-06 10:58:33 +0000

    Added PongGame example

--- Examples/PongGame/main.cpp
+++ Examples/PongGame/main.cpp
@@ -7,22 +7,10 @@
 //
 #include <Axilya/AXMain.h>
 
-int x = 0;
-void update(){
-    if(Input::getValue("D")){
-        x++;
-    }
-}
-
-void draw(){
-    //using the AXGraphics namespace you can draw raw shapes, lines and points 
-    AXGraphics::drawRect(x, 50, 50, 50);
-}
-
 int main(int argc, char *argv[])
 {
     //You can use axilya like a framework by passing the window update and draw methods
-    if(!AXWindow::init(720, 480, AX_WINDOWED, "Custom Update Draw", update, draw)){
+    if(!AXWindow::init(720, 480, AX_WINDOWED, "Pong Game", update, draw)){
     	std::cout << "AXWindow failed to initialise" << std::endl;
     	return -1;
     }
