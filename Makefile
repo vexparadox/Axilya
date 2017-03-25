a93e5f8315895cc6712c4b6453c46e1ae57bd79a
William Meaton <will.meaton@gmail.com>
2017-03-25 15:46:32 +0000

    removed SDL includes, will use the script instead

--- Makefile
+++ Makefile
@@ -1,13 +1,11 @@
 OS = $(shell uname)
 ifeq ($(OS), Linux)
 	CPFLAGS += `curl-config --libs` -I/usr/local/include/SDL2 -lSDL2_mixer -lSDL2_ttf
-	SDLRUN := @echo "SDL install isn't supported from makefile on Linux, got to https://github.com/vexparadox/Axilya/wiki/Dependencies to see how to install the required libraries"
+	SDLRUN := ./LinuxDependenciesInstall.sh
 else
 ifeq ($(OS), Darwin)
 	CPFLAGS += -mmacosx-version-min=10.9 -F/Library/Frameworks/
-	SDLRUN := @echo "Unpacking SDL2.0.5"; cd ./SDL && unzip -qq ./SDL2-osx.zip; echo "Copying SDL Framework";  cp -rf ./SDL2.framework /Library/Frameworks/; echo "Cleaning up..."; rm -rf ./SDL2.framework; echo "Install complete";
-	SDLRUN += echo "Unpacking SDL TTF"; unzip -qq ./SDL2_ttf-osx.zip; echo "Copying SDL TTF Framework";  cp -rf ./SDL2_ttf.framework /Library/Frameworks/; echo "Cleaning up..."; rm -rf ./SDL2_ttf.framework; echo "Install complete";
-	SDLRUN += echo "Unpacking SDL Mixer"; unzip -qq ./SDL2_mixer-osx.zip; echo "Copying SDL mixer Framework";  cp -rf ./SDL2_mixer.framework /Library/Frameworks/; echo "Cleaning up..."; rm -rf ./SDL2_mixer.framework; echo "Install complete";
+	SDLRUN := ./OSXDependenciesInstall.sh
 endif
 endif
 temp = $(wildcard ./src/*.cpp)
---
+++ OSXDependenciesInstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+mkdir temp
+cd temp
+echo "This script will download and install SDL2, SDL2 TTF and SDL2 Mixer"
+echo "Your password is required because we're installing to the system directory."
+#download files
+sudo echo "Downloading SDL2"
+curl https://www.libsdl.org/release/SDL2-2.0.5.dmg -o SDL2-2.0.5.dmg
+echo "Downloading SDL2 TTF"
+curl https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.14.dmg -o SDL2_ttf-2.0.14.dmg
+echo "Downloading SDL2 Mixer"
+curl https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.dmg -o SDL2_mixer-2.0.1.dmg
+
+#install sdl2
+echo "Copying SDL2"
+hdiutil attach -quiet -noverify SDL2-2.0.5.dmg
+sudo cp -r /Volumes/SDL2/SDL2.framework /Library/Frameworks
+echo "Copy complete"
+diskutil unmount /Volumes/SDL2
+#install sdl2 TTF
+echo "Copying SDL2 ttf"
+hdiutil attach -quiet -noverify SDL2_ttf-2.0.14.dmg
+sudo cp -r /Volumes/SDL2_ttf/SDL2_ttf.framework /Library/Frameworks
+echo "Copy complete"
+diskutil unmount /Volumes/SDL2_ttf
+#install sdl2 mixer
+echo "Copying SDL2 mixer"
+hdiutil attach -quiet -noverify SDL2_mixer-2.0.1.dmg
+sudo cp -r /Volumes/SDL2_mixer/SDL2_mixer.framework /Library/Frameworks
+echo "Copy complete"
+diskutil unmount /Volumes/SDL2_mixer
+cd ../
+rm -rf temp
+echo "Install complete."
